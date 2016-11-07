2b092a3ded078b720d4c35f01d81417a2a6ed2bb
Eser Ozvataf <eser@ozvataf.com>
2016-11-07 11:17:41 +0300

    Fixed a minor publish issue.

---
+++ makefile.js
@@ -0,0 +1,28 @@
+jsmake.desc('Bumps the package version for next release.');
+jsmake.task('bump', function (argv) {
+    jsmake.utils.packageJsonVersionBump('./package.json', argv.type || 'patch')
+        .then((version) => {
+            console.log('Bumped to version ' + version + '.');
+        });
+});
+
+jsmake.tasks.bump.parameters.setRule(
+    'type',
+    {
+        type: String,
+        description: 'Increment type [major, minor, patch, premajor, preminor, prepatch or prerelease]'
+    }
+);
+
+jsmake.desc('Publishes package to npm.');
+jsmake.task('publish', function (argv) {
+    jsmake.utils.npmPublish();
+});
+
+jsmake.desc('Reinstalls dependencies from npm.');
+jsmake.task('deps', function (argv) {
+    jsmake.utils.fs.rmdirP('node_modules')
+        .then(() => {
+            jsmake.utils.os.shell('npm install');
+        });
+});
